abfe40eb6647bd1a214bc38e76cb420c39e6482f
Наталья Никулина <natalanikulina@MacBook-Air-Natala.local>
2021-12-22 14:09:52 +0200

    new_unit

---
+++ Unit/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+namespace Unit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Input the operation: ");
+                    
+            char str = Console.ReadLine().Trim()[0];
+            double num1;
+            double num2;
+            switch(str)
+            {
+                case '+':
+                    Console.WriteLine("Input the first number: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Input the second number: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine( SimpleCalculator.Sum( num1, num2));
+                    break;
+                case '-':
+                    Console.WriteLine("Input the first number: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Input the second number: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(SimpleCalculator.Minus(num1, num2));
+                    break;
+                case '*':
+                    Console.WriteLine("Input the first number: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Input the second number: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(SimpleCalculator.Multiplay(num1, num2));
+                    break;
+                case '/':
+                    Console.WriteLine("Input the first number: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Input the second number: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(SimpleCalculator.Delen(num1, num2));
+                    break;
+                case '^':
+                    Console.WriteLine("Input the number: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(SimpleCalculator.KvadrKoren(num1));
+                    break;
+            }
+           
+        }
+
+
+
+
+        
+        
+    }
+}
---
+++ Unit/SimpleCalcul.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Unit
+{
+    public class SimpleCalculator
+    {
+
+
+            public static double Sum(double a, double b)
+
+            {
+                return a + b;
+            }
+            public static double Minus(double a, double b)
+            {
+                return a - b;
+            }
+            public static double Multiplay(double a, double b)
+            {
+
+                return a * b;
+
+            }
+            public static double Delen(double a, double b)
+            {
+                if (b == 0)
+                {
+                    throw new DivideByZeroException("Делить на ноль нельзя");
+                }
+                return a / b;
+            }
+            public static double KvadrKoren(double a)
+
+            {
+                if (a < 0)
+                {
+                    throw new ArgumentException("Корень из отрицательного не возможен");
+                }
+                return Math.Sqrt(a);
+            }
+
+        
+    }
+}
